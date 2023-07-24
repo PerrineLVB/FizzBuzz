2420ce40f6ea233e05fb49a142ca1ef5ce1e1070
PerrineLVB <perrine.launay@wanadoo.fr>
2023-07-24 11:33:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+for (let nb = 1; nb < 100; nb++) {
+    if (nb % 3 == 0 && nb % 5 == 0) {
+        console.log('FizzBuzz')
+    } else if (nb % 3 == 0){
+        console.log('Fizz')
+    } else if (nb % 5 == 0) {
+        console.log('Buzz')
+    } else {
+        console.log(nb)
+    }
+}
